f81f050fef1904336fd6cce767591706d21c359f
Nina <nina.bernardino05@gmail.com>
2025-04-30 01:57:15 -0400

    feat: added FAQ accordion

---
+++ itis3135/client_project/scripts/script.js
@@ -0,0 +1,26 @@
+const faqQuestions = document.querySelectorAll(".faq_question");
+
+faqQuestions.forEach((button) => {
+    button.addEventListener("click", () => {
+        const answer = button.nextElementSibling;
+        const isExpanded = answer.style.maxHeight && answer.style.maxHeight !== "0px";
+
+        // Collapse all other answers (optional accordion behavior)
+        faqQuestions.forEach((btn) => {
+            const ans = btn.nextElementSibling;
+            if (ans !== answer) {
+                ans.style.maxHeight = null;
+                btn.classList.remove("active");
+            }
+        });
+
+        if (isExpanded) {
+            answer.style.maxHeight = null;
+            button.classList.remove("active");
+        } else {
+            answer.style.maxHeight = answer.scrollHeight + "px";
+            button.classList.add("active");
+        }
+    });
+});
+
