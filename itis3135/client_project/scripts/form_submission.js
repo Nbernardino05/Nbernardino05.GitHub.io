bad74952ceb6d0f63eb41dc83c4af32e9f4543a3
Nina <nina.bernardino05@gmail.com>
2025-05-02 09:02:48 -0400

    feat: added form script

---
+++ itis3135/client_project/scripts/form_submission.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const forms = document.querySelectorAll("form");
+  
+    forms.forEach((form) => {
+      form.addEventListener("submit", (event) => {
+        event.preventDefault(); // prevent default form submission
+  
+        // hide the form
+        form.style.display = "none";
+  
+        // get the message that comes right after the form
+        const submissionMessage = form.nextElementSibling;
+  
+        if (submissionMessage && submissionMessage.classList.contains("hidden_message")) {
+            submissionMessage.classList.remove("hidden_message");
+        }
+      });
+    });
+  });
